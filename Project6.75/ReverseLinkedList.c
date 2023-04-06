eeaff0d6051292e8dda1e2eeca9489011d39d5f9
VolkovK04 <112119724+VolkovK04@users.noreply.github.com>
2023-04-06 05:37:46 +0300

    Update ReverseLinkedList.c

--- Project6.75/ReverseLinkedList.c
+++ Project6.75/ReverseLinkedList.c
@@ -11,39 +11,37 @@ struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
 	if (left == right) {
 		return head;
 	}
-	if (left + 1 == right) {
-		struct ListNode* buff = head;
-		head = head->next;
-		head->next = buff;
-		return head;
-	}
 	struct ListNode* ptr = head;
 	int index = 0;
-	while (index < left-2) {
+	while (index < left - 2) {
 		ptr = ptr->next;
 		index++;
+	}
+	struct ListNode newNode = {
+		.val = NULL,
+		.next = ptr
+	};
+	if (left == 1) {
+		ptr = &newNode;
+		index--;
 	}
 	struct ListNode* start = ptr;
 	ptr = ptr->next;
 	struct ListNode* end = ptr;
-
 	struct ListNode* buff1 = ptr;
 	ptr = ptr->next;
-	while (index++ != right-2) {
+	while (index++ != right - 2) {
 		struct ListNode* buff2 = ptr->next;
 		ptr->next = buff1;
 		buff1 = ptr;
 		ptr = buff2;
 	}
-	if (left == 1) {
-
-	}
-	else {
-		start->next = buff1;
-	}
 	end->next = ptr;
 	if (left == 1) {
 		return buff1;
+	}
+	else {
+		start->next = buff1;
 	}
 	return head;
 }
